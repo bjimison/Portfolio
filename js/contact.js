00c7c09fa5826dc4839cb9c1e44703cb7f4bd153
bjimison <brady.t.jimison@gmail.com>
2018-10-02 19:40:28 -0700

    website

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+document.getElementById('send').onmouseover = function(){
+  document.getElementById('showNote').style.display = 'block';
+}
+
+document.getElementById('send').onmouseout = function(){
+  document.getElementById('showNote').style.display = 'none';
+}
